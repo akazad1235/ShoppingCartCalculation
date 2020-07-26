e7db482cd2f9f7599f8b4e1df0097228bd5f4e4c
akazad1235 <akazad914@gmail.com>
2020-07-26 08:31:13 +0600

    Prodcut Quaatity Increase & Decrease $ Price Calculation Added

---
+++ js/apps.js
@@ -0,0 +1,22 @@
+//decliar and get ids
+
+const addBtn = document.getElementById('addBtn');
+const input1 = document.getElementById('input1');
+const minusBtn = document.getElementById('minusBtn')
+const phonePrice = document.getElementById("PhonePrice")
+
+
+    addBtn.addEventListener('click', function(event){
+        //plusButton(input1)
+
+        var getValue = parseInt(input.value)
+            getValue=getValue+1
+            input.value=getValue
+            //totalPrice = getValue+currentPriceNumber
+           
+        
+    })
+    minusBtn.addEventListener('click', function(event){
+        minusButton(input1)
+    
+    })
---
+++ js/functions.js
@@ -0,0 +1,16 @@
+   //plus 1 button funciton added
+    function plusButton(input, currentPriceNumber){
+        // var getValue = parseInt(input.value)
+        //     getValue=getValue+1
+        //     input.value=getValue
+        //     totalPrice = getValue+currentPriceNumber
+        //     return totalPrice
+        
+    }
+    function minusButton(input){
+        var getValues = parseInt(input.value)
+            getValues-=1
+            input.value=getValues   
+    }
+    
+   
